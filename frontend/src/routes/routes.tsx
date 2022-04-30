ed7de2475fb30a953a984fdf92e6ce131be7851a
Mary Skywalker <snowdennaruto90@gmail.com>
2022-04-29 22:16:03 -0400

    private route

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -1,8 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Outlet, Navigate } from 'react-router-dom';
 import { LogIn } from '../pages/LogIn';
 import { SignUp } from '../pages/SignUp';
 import { Chat } from '../pages/Chat';
 import { NotFound } from '../pages/NotFound';
+
+function RequireAuth(){
+    const useAuth = () => {
+        const user = localStorage.getItem('@chat:User');
+        if (user) {
+            return true;
+        } else {
+            return false
+        }
+    }
+
+    const auth = useAuth();
+    return auth ? <Outlet /> : <Navigate to="/" />
+}
 
 export function Rotas(){
     return(
@@ -10,7 +24,9 @@ export function Rotas(){
             <Routes>
                 <Route path="/" element={<LogIn />} />
                 <Route path="/signUp" element={<SignUp />} />
-                <Route path="/chat" element={<Chat />} />
+                <Route path="/" element={<RequireAuth/>}>
+                    <Route path="/chat" element={<Chat />} />
+                </Route>
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </Router>
