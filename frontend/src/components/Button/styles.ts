6faf33c3333e6621136f6522993f98500700cc35
Mary Skywalker <snowdennaruto90@gmail.com>
2022-05-03 22:22:42 -0400

    button linear-gradient

--- frontend/src/components/Button/styles.ts
+++ frontend/src/components/Button/styles.ts
@@ -8,11 +8,21 @@ export const Button = styled.button`
   cursor: pointer;
   font-size: 16px;
   font-family: "Poppins", sans-serif;
-  background: linear-gradient(
-    90deg,
-    rgba(231, 71, 220, 1) 0%,
-    rgba(255, 207, 0, 1) 100%
-  );
+  background: linear-gradient(to right, #833ab4, #fd1d1d, #fcb045);
+  background-size: 400% 400%;
+  animation: animate-background-button 10s infinite ease-in-out;
   color: #fff;
   margin-bottom: 20px;
+
+  @keyframes animate-background-button {
+    0% {
+      background-position: 0 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0 50%;
+    }
+  }
 `;
